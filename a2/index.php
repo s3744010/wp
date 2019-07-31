f93458d6ee6114770d4fb79e79b41eca3806d080
s3744010 <s3744010@student.rmit.edu.au>
2019-07-31 15:15:15 +1000

    Linking css styles with html

--- a2/index.php
+++ a2/index.php
@@ -7,7 +7,7 @@
     
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
-    <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
+    <link id='style.css' type="text/css" rel="stylesheet" href="css/style.css">
     <script src='../wireframe.js'></script>
   </head>
 
