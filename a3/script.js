72583e132b849304347ff8185e10778089da3166
s3744010 <s3744010@student.rmit.edu.au>
2019-09-13 15:48:23 +1000

    Adding a2 files

--- a3/script.js
+++ a3/script.js
@@ -1,1 +1,8 @@
 /* Insert your javascript here */
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 1)";
+    } else {
+        document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0)";
+    }
+}
