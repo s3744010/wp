703f2dfa29f70cc8f31eb9446a240a6446f0a74f
Ervin Teoh <s3744010@student.rmit.edu.au>
2019-09-24 17:20:34 +1000

    Now Showing and Sypnosis Programming

--- a3/script.js
+++ a3/script.js
@@ -7,3 +7,12 @@ function turnActive(element) {
     });
     element.classList.add("active");
 }
+
+function movieOption(element) {
+    var synopsis = document.querySelector(".synopsis");
+    document.getElementsByClassName('synopsis')[0].style.display = "flex";
+    synopsis.querySelector("h1").innerHTML = element.querySelector(".title p").innerHTML;
+    document.querySelector(".booking .time").innerHTML = element.querySelector(".time .inner").innerHTML;
+    synopsis.querySelector(".plot p").innerHTML = element.querySelector(".plot_description").innerHTML;
+    synopsis.querySelector(".trailer").innerHTML = element.querySelector(".trailer").innerHTML;
+}
