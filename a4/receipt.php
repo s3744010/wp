b5d04790d061c7b1cd304163e645dfedb1431d15
s3733738 <s3733738@student.rmit.edu.au>
2019-10-20 21:00:09 +1100

    receipt undone

--- a4/receipt.php
+++ a4/receipt.php
@@ -1,9 +1,148 @@
 <?php
-    include 'tools.php';
-    receiptPage();
+include 'tools.php';
+receiptPage();
+$subtotal = calSubTotal();
+
+echo $_SESSION[''];
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
+<style>
+    @import url("https://fonts.googleapis.com/css?family=Montserrat");
+    @import url("https://fonts.googleapis.com/css?family=Blinker&display=swap");
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
+    @font-face {
+        src: url(../../fonts/Bebas_Neue.ttf);
+        font-family: Bebas Neue;
+    }
+
+    :root {
+        --main-bg-color: #0B0C10;
+        --secondary-bg-color: #1F2833;
+        --main-cont-color: #66FCF1;
+        --secondary-cont-color: #45A29E;
+        --main-font-color: #C5C6C7;
+    }
+
+    .invoice {
+        display: block;
+        background: white;
+        border: 1px solid black;
+        height: auto;
+        width: auto;
+        padding: 100px;
+        margin: 80px;
+
+    }
+
+    .invoice .inner {
+        display: block;
+        width: 100%;
+        color: black;
+    }
+
+    .invoice .h1 {
+        display: grid;
+
+    }
+
+    .invoice .h2 {
+        display: block;
+        justify-content: left;
+    }
+
+    .invoice .inner table {
+        border-collapse: collapse;
+        width: 100%;
+        height: 200px;
+        box-shadow: 0 0 10px 2px #f5f5f5b2;
+    }
+
+    .invoice .inner table th,
+    td {
+        padding: 8px;
+        text-align: left;
+        font-family: "Montserrat", sans-serif;
+        font-size: 15px;
+    }
+
+    .invoice .inner .top {
+
+        font-size: 20px;
+
+    }
+
+    .ticket {
+
+        height: 100%;
+        width: 80%;
+        padding: 100px;
+
+
+
+    }
+
+    .ticket .container {
+        display: block;
+        background: #f8de7e;
+        height: 280px;
+        width: 600px;
+
+    }
+
+    .ticket .ticket-style {
+        position: relative;
+        font-family: "Montserrat", sans-serif;
+    }
+
+    .ticket .ticket-style h2 {
+        position: absolute;
+        top: 5px;
+        left: 400px;
+        padding: 20px;
+
+    }
+
+    .ticket .ticket-style .movie-name {
+        position: absolute;
+        top: 15px;
+        left: 10px;
+        padding: 30px;
+        font-size: 35px;
+    }
+
+    .ticket .ticket-style .ticket-type {
+        position: absolute;
+        top: 60px;
+        left: 10px;
+        padding: 30px;
+        font-size: 27px;
+
+    }
+
+    .ticket .ticket-style .details {
+        position: absolute;
+        top: 130px;
+        left: 10px;
+        padding: 30px;
+        font-size: 25px;
+
+    }
+
+    .ticket .ticket-style .details th,
+    td {
+        margin: 0px;
+        padding-left: 30px;
+        text-align: left;
+
+    }
+</style>
 
 <head>
     <meta charset="UTF-8">
@@ -14,6 +153,84 @@
 
 <body>
     <a href="./index.php">Back to front page</a>
+    <div class="invoice">
+        <div class="inner">
+            <h1>Tax Invoice</h1>
+            <h2>LUNARDO</h2>
+
+
+            <div class="top">
+                <a>ABN: 00 123 456 789</a>
+                <div class="movie-name"><?php echo  $_SESSION['movie']['id'] ?></div>
+                <div class="day">DAY: <?php echo  $_SESSION['movie']['day'] ?></div>
+                <div class="hour">TIME: <?php echo  $_SESSION['movie']['hour'] ?></div>
+            </div>
+
+            <div class="custInfo">
+                <?php echo  $_SESSION['cust']['name'] ?><br>
+                <?php echo  $_SESSION['cust']['email'] ?><br>
+                <?php echo  $_SESSION['cust']['mobile'] ?><br>
+            </div>
+
+            <div class="seat-deatails">
+                <table>
+                    <tr>
+                        <th>QTY</th>
+                        <th>ITEM</th>
+                        <th>PRICE</th>
+                        <th>SUBTOTAL</th>
+                    </tr>
+                    <tr>
+                        <td><?php echo $subtotal[0] ?></td>
+                        <td><?php echo preShow($_SESSION['seats']); ?></td>
+                    </tr>
+                    <tr>
+                        <td><?php echo  $_SESSION['seats']['STC'] ?></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+
+    </div>
+
+    <div class="ticket">
+        <h1>Ticket</h1>
+        <div class="container">
+
+            <div class="ticket-style">
+                <img src="../../media/ticketbg.png" alt="ticket">
+                <h2>LUNARDO</h2>
+                <br>
+                <div class="movie-name">DUMBO</div>
+                <br>
+                <div class="ticket-type"> ADULT STANDARD </div>
+
+                <div class="details">
+                    <table>
+                        <tr>
+                            <th>PRICE</th>
+                            <th>DATE</th>
+                            <th>TIME</th>
+                            <th>RATING</th>
+                        </tr>
+                        <tr>
+                            <td>$10</td>
+                            <td><?php echo  $_SESSION['movie']['day'] ?></td>
+                            <td><?php echo  $_SESSION['movie']['hour'] ?></td>
+                            <td>R</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <?= printTicket(); ?>
+        </div>
+        
+
+    </div>
+
+
+
 </body>
 
 </html>
--- a4/tools.php
+++ a4/tools.php
@@ -2,10 +2,67 @@
 
 session_start();
 
+function movies()
+{
+
+  $moviesObject = [
+    'ACT' => [
+      'title' => 'Avengers: Endgame',
+      'rating' => 'R',
+      'screenings' => [
+        'MON' => 'T18',
+        'TUE' => 'T18',
+        'SAT' => 'T15',
+        'SUN' => 'T15'
+      ]
+    ],
+
+    'RMC' => [
+      'title' => 'Top-End-Wedding',
+      'rating' => 'R',
+      'screenings' => [
+        'WED' => 'T21',
+        'THU' => 'T21',
+        'FRI' => 'T21',
+        'SAT' => 'T18',
+        'SUN' => 'T18'
+      ]
+    ],
+
+    'ANM' => [
+      'title' => 'Dumbo',
+      'rating' => 'R',
+      'screenings' => [
+        'MON' => 'T12',
+        'TUE' => 'T12',
+        'WED' => 'T18',
+        'THU' => 'T18',
+        'FRI' => 'T21',
+        'SAT' => 'T12',
+        'SUN' => 'T12'
+      ]
+    ],
+
+    'AHF' => [
+      'title' => 'The Happy Prince',
+      'rating' => 'R',
+      'screenings' => [
+        'WED' => 'T12',
+        'THU' => 'T12',
+        'FRI' => 'T12',
+        'SAT' => 'T21',
+        'SUN' => 'T21'
+      ]
+    ]
+  ];
+}
+
+
 function indexPage()
 {
-  if ($_POST) {
+  if (!empty($_POST)) {
     $_SESSION['seats'] = $_POST['seats'];
+    $_SESSION['movie'] = $_POST['movie'];
     $_SESSION['cust'] = $_POST['cust'];
 
     $date = date("Y/m/d");
@@ -80,6 +137,98 @@ function receiptPage()
     $_POST = array();
   }
 }
+
+function printTicket()
+{
+  $seats = $_SESSION['seats'];
+  foreach ($seats as $seatType => $value) {
+    for ($index = 0; $index < $value && $seatType != "totalPrice"; $index++) {
+
+      echo  '<div class="ticket-style">
+                <img src="../../media/ticketbg.png" alt="ticket">
+                <h2>LUNARDO</h2>
+                <br>
+                <div class="movie-name">DUMBO</div>
+                <br>
+                <div class="ticket-type">'.  $seatType . '</div>
+
+                <div class="details">
+                    <table>
+                        <tr>
+                            <th>PRICE</th>
+                            <th>DATE</th>
+                            <th>TIME</th>
+                            <th>RATING</th>
+                        </tr>
+                        <tr>
+                            <td>$10</td>
+                            <td>' .  $_SESSION['movie']['day'] . '</td>
+                            <td>' . $_SESSION['movie']['hour'] . '</td>
+                            <td>R</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>';
+      // echo $seatType . $value . "\n\n";
+    }
+  }
+}
+
+
+function calSubTotal()
+{
+
+
+  function isFullOrDiscount($day, $hour)
+  {
+
+    $ret = "full";
+    if ($day != "Sat" && $day != "Sun")
+      $ret = "discount";
+
+    if ($hour != "12PM" && $day != "Mon" && $day != "Wed")
+      $ret = "full";
+    return $ret;
+  }
+
+  function calcResult()
+  {
+    $ret = isFullOrDiscount($_POST['movie']['day'], $_POST['movie']['hour']);
+    $subtotals = array();
+    $seatsQty = $_SESSION['seats'];
+
+    $prices = [
+      'full' => [
+        'STA' => 19.8,
+        'STP' => 17.5,
+        'STC' => 15.30,
+        'FCA' => 30.00,
+        'FCP' => 27.00,
+        'FCC' => 24.00
+
+      ],
+      'discount' => [
+        'FCA' => 24.00,
+        'FCP' => 22.50,
+        'FCC' => 21.00,
+        'STA' => 14.00,
+        'STP' => 12.50,
+        'STC' => 11.00
+      ]
+    ];
+
+    // for ($value = 0; $value < 6; $value++) {
+    //   array_push($subtotals, $seatsQty[$value] * $prices[$ret][$value]);
+    // }
+    foreach ($prices[strval($ret)] as $index => $price) {
+      array_push($subtotals, $seatsQty[$index] * $price);
+    }
+    // return $subtotals;
+    return $subtotals;
+  }
+  return calcResult();
+}
+
 
 function preShow($arr, $returnAsString = false)
 {
